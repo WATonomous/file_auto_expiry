b6a7fbaf0589205d785e856539cd4971fe7a63ce
cehune <mckale.chung@gmail.com>
2024-02-21 17:12:25 +0000

    Updated changes, only notifys top level folders but checks all files

--- infra_file_auto_expiry/source/main.py
+++ infra_file_auto_expiry/source/main.py
@@ -3,12 +3,15 @@ import sys
 
 
 def main(base_folder):
-    delete_expired_files(base_folder)
+    delete_expired_files(base_folder, temp_folder, 0)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python file_expiry.py <folder to scan>")
+    if len(sys.argv) != 3:
+        
+        print("Usage: python file_expiry.py <folder to scan> <temp>")
         sys.exit(1)
 
     base_folder = sys.argv[1]
+    temp_folder = sys.argv[2]
+
     main(base_folder=base_folder)
--- infra_file_auto_expiry/source/utils.py
+++ infra_file_auto_expiry/source/utils.py
@@ -1,46 +1,9 @@
 import pwd
-import subprocess
 import shutil
 import os
-import datetime
 import time
-import random
-import string
 
-def generate_folder(base_path, permission=700):
-    """
-    Generates a new folder with the specified permissions
-
-    string base_path: parent folder to generate the new folder
-    int permission: The permission code for the folder. Default is 700,
-        meaning that the folder is accessible by root only
-    """
-    # Generate folder name
-    folder_name = datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S')
-    folder_path = os.path.join(base_path, folder_name)
-
-    # Re run above code until it is confirmed we have a new folder
-    while os.path.isdir(folder_path):
-        folder_name = datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S')
-        unique_random_key = "".join(random.choices(string.ascii_letters + string.digits, k=5))
-        folder_path = os.path.join(base_path, f"{folder_name}{unique_random_key}")
-    
-    # generate folder
-    os.makedirs(folder_path)
-    
-    # modify folder settings permissions
-    try:
-        print("")
-        #subprocess.run(["sudo", "chmod", str(permission), folder_path], check=True)
-    except Exception as e:
-        # Catch for errors, likely from non existing permission code
-        print(e)
-        os.rmdir(folder_path)
-        return None
-
-    return folder_path
-
-def is_expired(path, days_for_expire=30):
+def is_expired_file(path, days_for_expire=30):
     """
     Checks the last time a file or folder has been accessed. If it has not 
     been accessed in the days specified, then return True. False if otherwise.
@@ -50,16 +13,41 @@ def is_expired(path, days_for_expire=30):
         has expired. 
     """
     current_time = time.time()
-
     # get last access time (includes change and modification times)
     file_stat = os.stat(path)
-    last_access = file_stat.st_atime
-
-    # Generate amount of time since last access
-    seconds_last_access = (current_time - last_access) 
 
     # compare days for expiry (in seconds) to the time since last access
-    return (days_for_expire * 24 * 60 * 60) < seconds_last_access
+    return ((days_for_expire * 24 * 60 * 60) < (current_time - file_stat.st_atime) and
+        (days_for_expire * 24 * 60 * 60) < (current_time - file_stat.st_ctime) and 
+        (days_for_expire * 24 * 60 * 60) < (current_time - file_stat.st_mtime))
+
+def is_expired_folder(folder_path, days_for_expire=30):
+    """
+    Goes through all files in a folder, and deletes the ones that
+    have expired
+
+    string base_folder: The folder containing the files to delete
+    """
+    if not os.path.isdir(folder_path):
+        print("Bad folder path:", folder_path)
+        return False
+    
+    all_files_expired = True
+    for entry in os.scandir(folder_path):
+        if entry.is_dir():
+            if not is_expired_folder(entry.path, days_for_expire):
+                return False
+        elif entry.is_file() and not is_expired(entry.path, days_for_expire):
+            all_files_expired = False
+    return all_files_expired
+
+def is_expired(path, days_for_expire=30):
+    if os.path.isfile(path):
+        return is_expired_file(path, days_for_expire)
+    if os.path.isdir(path):
+        return is_expired_folder(path, days_for_expire)
+    
+    return False
 
 def get_file_creator(path):
     """
@@ -90,42 +78,17 @@ def notify_file_creator(file_creator):
     """
     print(f"Deleting file by ", file_creator)
 
-def find_expired_files(folder_path):
-    """
-    Goes through all files in a folder, and deletes the ones that
-    have expired
+def scan_folder_for_expired(folder_path, days_for_expire=30):
+    for entry in os.scandir(folder_path):
+        if is_expired(entry.path, days_for_expire):
+            yield entry.path 
 
-    string base_folder: The folder containing the files to delete
-    """
-    if not os.path.isdir(folder_path):
+def delete_expired_files(folder_path, temp_folder, days_for_expire=30):
+    if not os.path.isdir(folder_path) or not os.path.isdir(temp_folder):
         print("Base folder does not exist")
         return
     
-    with os.scandir(folder_path) as entries:
-        for entry in entries:
-            if entry.is_file() or entry.is_dir():
-                entry_path = os.path.join(folder_path, entry.name)
-                if is_expired(entry_path):
-                    entry_path_no_ext, ext = os.path.splitext(entry_path)
-                    unique_timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S')
-                    unique_random_key = "".join(random.choices(string.ascii_letters + string.digits, k=5))
-                    entry_path_unique = f"{entry_path_no_ext}_{unique_timestamp}_{unique_random_key}{ext}"
-                    
-                    os.rename(entry_path, entry_path_unique)
-                    yield entry_path_unique
-
-def delete_expired_files(folder_path):
-    if not os.path.isdir(folder_path):
-        print("Base folder does not exist")
-        return
-    
-    temp_folder = generate_folder(os.path.dirname(folder_path))
-    if temp_folder is None:
-        print("Unable to create a temporary folder, cancelling file deletion")
-        return
-
-    for path in find_expired_files(folder_path):
-        
+    for path in scan_folder_for_expired(folder_path, days_for_expire):
         notify_file_creator(get_file_creator(path))
         shutil.move(path, temp_folder)
     
