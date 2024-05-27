e48f9ac82d94d01d09bf937db24027a470b5de5c
cehune <mckale.chung@gmail.com>
2024-05-27 23:35:28 +0000

    Optimizing imports for main.py

--- source/main.py
+++ source/main.py
@@ -1,4 +1,5 @@
 from utils.interface import *
+from data.expiry_constants import SECS_PER_DAY
 import time
 import typer
 app = typer.Typer()
@@ -10,12 +11,12 @@ def collect_file_info(path: str, save_file: str = "", days_for_expiry: int = 10)
     And dumps it into a json file, specified by the save_file flag
     """
     scrape_time = time.time()
-    seconds_for_expiry = int(days_for_expiry) * 3600 * 24
+    seconds_for_expiry = int(days_for_expiry) * SECS_PER_DAY
     expiry_threshold = scrape_time - seconds_for_expiry
     collect_expired_file_information(folder_path=path, 
                                      save_file=save_file, 
                                      scrape_time=scrape_time, 
-                                     seconds_for_expiry=seconds_for_expiry)
+                                     expiry_threshold=expiry_threshold)
 
 @app.command()
 def collect_creator_info(file_info: str, save_file: str = ""):
