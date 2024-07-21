de545391006771f87a4ab376fecc98699971176b
cehune <mckale.chung@gmail.com>
2024-07-21 17:36:36 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="wato_file_auto_expiry", 
+    version="0.0.1", 
+    description="WATCloud project containing scripts to check if directories / files are expired",  # Optional
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Programming Language :: Python :: 3",
+    ],
+    package_dir={"": "source"},  
+    packages=find_packages(where="source"), 
+   
+    python_requires=">=3.7, <4",
+    extras_require={ 
+        "dev": ["check-manifest"],
+        "test": ["coverage"],
+    }
+)
